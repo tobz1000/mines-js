6a487dd3d4b1e684a6c8f5fc5150c3042c6ca009
Toby Dimmick <tobydimmick@protonmail.ch>
2016-09-28 18:20:22 +0100

    Converted ClientGame/GameCell to class syntax
    Also converted serverAction to use new server paths (/entity/action instead
    of /entity with body={action:...)
    
    Also using async functions in client.js now (supported in Chrome
    experimental JS)

--- src/public/client.js
+++ src/public/client.js
@@ -13,17 +13,19 @@ $(() => {
 	$gameList = $("#gameList ul");
 	$gameArea.on('contextmenu', (e) => { e.preventDefault() });
 
-	setInterval(() => {
-		$.getJSON("server/games", null, refreshGameList);
-	}, 1000);
+	$("#gameListRefresh").click(refreshGameList);
+	refreshGameList();
 });
 
-const refreshGameList = games => {
+const refreshGameList = async () => {
+	const games = await $.getJSON("server/games");
+
 	$gameList.empty();
 
 	for(const g of games) {
-		/* TODO: race condition for display of "watchable"/"playable", if the
-		response from newGame() is received after the gameLister entry. */
+		/* TODO: race condition for display of "watchable"/"playable", if
+		the response from newGame() is received after the gameLister entry.
+		*/
 		const label = `${g.id} (${g.dims[0]}x${g.dims[1]}, ${g.mines}, ` +
 				`${gamePasses[g.id] ? "playable" : "watchable"})`;
 		$gameList.append($("<li>")
@@ -44,7 +46,8 @@ const newGame = () => {
 	const pass = Math.random().toString(36).substr(2, 10);
 
 	serverAction(
-		{ action: 'new', dims: [x, y], mines: mines, pass: pass },
+		"new",
+		{ dims: [x, y], mines: mines, pass: pass },
 		resp => {
 			gamePasses[resp.id] = pass;
 			displayGame(resp, pass);
@@ -90,92 +93,123 @@ const showMsg = msg => {
 
 /* Send a request to the server; optionally perform an action based on the
 response. */
-const serverAction = (req, respFn) => {
+const serverAction = async (action, req, respFn) => {
 	/* TODO - proper 'fail' handler, once the server gives proper HTTP codes */
-	$.post('server/' + req.action, JSON.stringify(req), resp => {
-		if(resp.error) {
-			let errMsg = `Server error: ${resp.error}`;
-			if(resp.info)
-				errMsg += `\nInfo: ${JSON.stringify(resp.info)}`;
-			showMsg(errMsg);
-			return;
-		}
+	const resp = JSON.parse(
+		await $.post('server/' + action, JSON.stringify(req))
+	);
 
-		if(respFn)
-			respFn(resp);
-	}, 'json');
-};
-
-/* Optional 'pass' param if game is controllable */
-const ClientGame = function(id, dims, mines, pass, debug) {
-	const cellState = {
-		UNKNOWN : "u",
-		FLAGGED : "f"
+	if(resp.error) {
+		let errMsg = `Server error: ${resp.error}`;
+		if(resp.info)
+			errMsg += `\nInfo: ${JSON.stringify(resp.info)}`;
+		showMsg(errMsg);
+		return;
 	}
 
-	const serverWatcher = new EventSource(`server/watch?id=${id}&from=0`);
+	if(respFn)
+		respFn(resp);
+};
 
-	/* Representation of game state; each cell is a 'GameCell'. */
-	const gameGrid = [];
-	/* List of lists of cellDatas, to represent each turn in the game. */
-	const gameTurns = {};
-	/* Retroactive list of cells the client intended to clear next, for each
-	previous turn in the game. */
-	const toClearCoords = {};
-	/* Retroactive list of flagged cells, extracted from client debug. Info from
-	last available turn is used for latest turn, since latest turn's debug won't
-	be available. */
-	/* TODO: refactor this and toClearCoords into a table of info for "flagged"
-	and "toClear", sharing code functionality. */
-	const flaggedCoords = {};
-	/* Debug info for each turn */
-	const debugInfo = {};
-	let currentTurn = -1;
-	let latestTurn;
-	let gameOver = false;
+const cellState = {
+	UNKNOWN : "u",
+	FLAGGED : "f"
+};
 
-	serverAction(
-		{ action: 'status', id: id },
-		resp => {
-			latestTurn = resp.turn;
-			displayTurn(latestTurn);
+class ClientGame {
+	constructor(id, dims, mines, pass, showDebug) {
+		if(dims.length !== 2)
+			throw new Error("Only 2d games supported");
+
+		$.extend(this, {id, dims, mines, pass, showDebug});
+
+		this.serverWatcher = new EventSource(`server/watch?id=${id}&from=0`);
+		this.serverWatcher.addEventListener("message", (resp) => {
+			this.updateTurnList(JSON.parse(resp.data));
+		});
+		showDebug && this.serverWatcher.addEventListener("debug", (resp) => {
+			this.updateDebug(JSON.parse(resp.data));
+		});
+
+		this.gameTurns = {};
+		this.toClearCoords = {};
+		this.flaggedCoords = {};
+		this.debugInfo = {};
+		this.currentTurn = -1;
+		this.gameOver = false;
+
+		/* Retrieve turn number from /status, so we know when the final SSE
+		 * message has been received */
+		serverAction("status", { id: id }, resp => {
+			this.latestTurn = resp.turn;
+			this.displayTurn(this.latestTurn);
+		});
+
+		this.newGameTable();
+
+		$gameArea.append($("<ol>").attr("id", "turnList").addClass("laminate"));
+		$gameArea.append(
+			$("<div>").attr("id", "debugArea").append(
+				$("<div>").attr("id", "debugAreaTurn"),
+				$("<div>").attr("id", "debugAreaCell")
+			)
+		);
+	}
+
+	newGameTable() {
+		const $gameTable = $("<table>");
+		const gameGrid = [];
+
+		for(let i = 0; i < this.dims[0]; i++) {
+			gameGrid[i] = [];
+			let $row = $("<tr>");
+			$gameTable.append($row);
+
+			for(let j = 0; j < this.dims[1]; j++) {
+				gameGrid[i][j] = new GameCell(this, [i, j]);
+				$row.append(gameGrid[i][j].$elm);
+			}
 		}
-	)
 
-	const displayTurn = newTurn => {
+		this.$gameTable = $gameTable;
+		this.gameGrid = gameGrid;
+	}
+
+	displayTurn(newTurn) {
 		/* When loading a new game, don't render anything until data for the
 		latest turn has been loaded (and the number of the latest turn is
 		retrieved). */
-		if(latestTurn === undefined || !gameTurns[latestTurn])
+		if(this.latestTurn === undefined || !this.gameTurns[this.latestTurn])
 			return;
 
-		$gameTable.detach();
-		const reverse = newTurn < currentTurn;
-		const start = (reverse ? newTurn : currentTurn) + 1;
-		const end = reverse ? currentTurn : newTurn;
+		this.$gameTable.detach();
+		const reverse = newTurn < this.currentTurn;
+		const start = (reverse ? newTurn : this.currentTurn) + 1;
+		const end = reverse ? this.currentTurn : newTurn;
 
 		/* Reset any highlighted "to clear" cells if going backwards. */
-		if(reverse && toClearCoords[currentTurn]) {
-			for (let coords of toClearCoords[currentTurn])
-				getCell(coords).changeState('unknown');
+		if(reverse && this.toClearCoords[this.currentTurn]) {
+			for (let coords of this.toClearCoords[this.currentTurn])
+				this.getCell(coords).changeState('unknown');
 		}
 
 		/* Remove flags - even going forwards, in case the client allows
 		unflagging between turns. */
 		let flaggedList = (
-			flaggedCoords[currentTurn] ||
-			flaggedCoords[currentTurn - 1]
+			this.flaggedCoords[this.currentTurn] ||
+			this.flaggedCoords[this.currentTurn - 1]
 		);
+
 		if(flaggedList) {
 			for (let coords of flaggedList)
-				getCell(coords).changeState('unknown');
+				this.getCell(coords).changeState('unknown');
 		}
 
 		/* Set all cell data between old turn and new turn, or remove it if
 		going backwards */
 		for (let i = start; i <= end; i++) {
-			for (let cellData of gameTurns[i]) {
-				getCell(cellData.coords).changeState(
+			for (let cellData of this.gameTurns[i]) {
+				this.getCell(cellData.coords).changeState(
 					reverse ? 'unknown' : cellData.state,
 					cellData.surrounding
 				);
@@ -183,203 +217,45 @@ const ClientGame = function(id, dims, mines, pass, debug) {
 		}
 
 		/* Set new "to clear" cells */
-		if(toClearCoords[newTurn]) {
-			for (let coords of toClearCoords[newTurn])
-				getCell(coords).changeState('toClear');
+		if(this.toClearCoords[newTurn]) {
+			for (let coords of this.toClearCoords[newTurn])
+				this.getCell(coords).changeState('toClear');
 		}
 
 		/* Show flagged mines from client's debug. Show previous turn's flags if
 		the current turn's debug is unavailable. */
-		flaggedList = (flaggedCoords[newTurn] || flaggedCoords[newTurn - 1]);
+		flaggedList = (
+			this.flaggedCoords[newTurn] ||
+			this.flaggedCoords[newTurn - 1]
+		);
+
 		if(flaggedList) {
 			for (let coords of flaggedList)
-				getCell(coords).changeState('flagged');
+				this.getCell(coords).changeState('flagged');
 		}
 
-		$gameArea.prepend($gameTable);
+		$gameArea.prepend(this.$gameTable);
 
 		displayDebug(
 			$("#debugAreaTurn"),
-			() => debugInfo[newTurn].gameInfo
+			() => this.debugInfo[newTurn].gameInfo
 		);
 
 		/* Remove cell debug display */
 		displayDebug($("#debugAreaCell"));
 
-		currentTurn = newTurn;
+		this.currentTurn = newTurn;
 	}
 
-	/* Perform a turn: send request to server */
-	/* TODO: send list of currently flagged cell to server, so they can be
-	displayed properly when the game is played back. */
-	const clearCells = coordsArr => {
-		if(!pass)
-			throw new Error(`Don't have the password for game '${id}'`);
-
-		serverAction({
-			action : 'turn',
-			id : id,
-			pass: pass,
-			coords : coordsArr
-		});
-	};
-
-	const getCell = coords => {
-		return gameGrid[coords[0]][coords[1]];
-	}
-
-	/* Disable user game actions when viewing a past turn, or someone else's
-	game */
-	const inPlayState = () => {
-		return pass && !gameOver && currentTurn === latestTurn;
-	}
-
-	const GameCell = function(coords) {
-		/* Get surrounding co-ordinates that aren't cleared or flagged. */
-		const surroundingUnknownCoords = () => {
-			const ret = [];
-			for (let i of [-1, 0, 1])
-				for (let j of [-1, 0, 1]) {
-					if(i === 0 && j === 0)
-						continue;
-
-					let x = coords[0] + i, y = coords[1] + j;
-
-					if(x < 0 || y < 0 || x > dims[0] - 1 || y > dims[1] - 1)
-						continue;
-
-					if(gameGrid[x][y].state !== cellState.UNKNOWN)
-						continue;
-
-					ret.push([x, y]);
-				}
-			return ret;
-		};
-
-		/* TODO: figure out a nice way to stop the flashing when the cursor
-		moves between two cells. Probably use border-collapse on the table, then
-		some other CSS to retain the white edges on cells. Or just use fancy
-		fading. */
-		const hoverSurrounding = hoverOn => {
-			for(const c of surroundingUnknownCoords()){
-				getCell(c).$elm.toggleClass("cellHover", hoverOn);
-			}
-		}
-
-		const clearSurrounding = () => {
-			const surrCoords = surroundingUnknownCoords();
-			if(surrCoords.length > 0)
-				clearCells(surrCoords);
-
-			hoverSurrounding(false);
-		}
-
-		const getDebug = () => {
-			return debugInfo[currentTurn].cellInfo[coords.toString()];
-		}
-
-		this.$elm = $("<td>")
-			.addClass("cell laminate");
-
-		/* Change state of one cell; perform internal data & GUI changes */
-		this.changeState = (newStateName, surrCount) => {
-			const states = {
-				flagged : {
-					cellState : cellState.FLAGGED,
-					class : 'cellFlagged',
-					contextmenu : () => { this.changeState('unknown'); },
-				},
-				mine : {
-					class : 'cellMine'
-				},
-				unknown : {
-					cellState : cellState.UNKNOWN,
-					class : 'cellUnknown',
-					click : () => {
-						this.$elm.removeClass('cellHover');
-						clearCells([coords]);
-					},
-					contextmenu : () => { this.changeState('flagged'); },
-					mouseover : () => { this.$elm.addClass('cellHover'); },
-					mouseout : () => { this.$elm.removeClass('cellHover'); }
-				},
-				cleared : {
-					cellState : surrCount,
-					class : 'cellCleared',
-					text : surrCount > 0 ? surrCount : undefined,
-					click : surrCount > 0 ? clearSurrounding : undefined,
-					mouseover : surrCount > 0 ?
-						() => { hoverSurrounding(true); } : undefined,
-					mouseout : surrCount > 0 ?
-						() => { hoverSurrounding(false); } : undefined
-				},
-				toClear : {
-					class : 'cellToClear'
-				}
-			};
-
-			const newState = states[newStateName];
-			if(!newState)
-				throw new Error(`unexpected cell state: "${newStateName}"`);
-
-			/* Reverse any current mouseover effect */
-			this.$elm.mouseout();
-			this.$elm.off();
-			this.$elm.text("");
-
-			for(const s in states)
-				if(s !== newStateName && states[s].class)
-					this.$elm.removeClass(states[s].class);
-
-			this.state = newState.cellState;
-			this.$elm.addClass(newState.class);
-			this.$elm.text(newState.text);
-
-			/* Apply mouse actions to cell */
-			for (let mouseAction of [
-				'click',
-				'contextmenu',
-				'mouseover',
-				'mouseout',
-				'mouseup'
-			]) {
-				if(newState[mouseAction]) {
-					this.$elm.on(mouseAction, () => {
-						if(inPlayState())
-							newState[mouseAction]();
-					});
-				}
-			}
-
-			if(debug) {
-				this.$elm.on('click', () => {
-					displayDebug($("#debugAreaCell"), getDebug);
-				});
-			}
-
-			/* TODO: this is meant to highlight surrounding cells right after
-			clicking an unknown cell. Doesn't work (:hover is false); don't know
-			why. */
-			// if(this.$elm.is(":hover"))
-			// 	this.$elm.mouseover();
-		};
-
-		this.changeState('unknown');
-	};
-
-	if(dims.length !== 2)
-		throw new Error("Only 2d games supported!");
-
-	serverWatcher.addEventListener('message', (resp) => {
-		const data = JSON.parse(resp.data);
-		const turnNumber = data.turn;
-
+	updateTurnList({turn : turnNumber, newCellData, gameOver, win}) {
+		console.log(arguments);
 		/* Add turn new data from server to list & GUI */
-		gameTurns[turnNumber] = data.newCellData;
+		this.gameTurns[turnNumber] = newCellData;
+
 		$("#turnList").append($("<li>")
 			.click(() => {
-				if(currentTurn !== turnNumber)
-					displayTurn(turnNumber);
+				if(this.currentTurn !== turnNumber)
+					this.displayTurn(turnNumber);
 			})
 			.text("Turn")
 			.attr("value", turnNumber)
@@ -387,61 +263,213 @@ const ClientGame = function(id, dims, mines, pass, debug) {
 
 		/* Wait for initial latestTurn value from server before attempting to
 		update it */
-		if(latestTurn !== undefined)
-			latestTurn = Math.max(latestTurn, turnNumber);
+		if(this.latestTurn !== undefined)
+			this.latestTurn = Math.max(this.latestTurn, turnNumber);
 
-		displayTurn(turnNumber);
+		this.displayTurn(turnNumber);
 
-		if(data.gameOver) {
-			gameOver = true;
-			showMsg(data.win ? "Win!!!1" : "Lose :(((");
-		}
-	});
-
-	debug && serverWatcher.addEventListener('debug', (resp) => {
-		const data = JSON.parse(resp.data);
-		const turnNumber = data.turn;
-		/* How the client indicates a cell is flagged (v. specific to python
-		client). */
-		const flaggedIndicator = info => info._state == "State.MINE";
-
-		debugInfo[turnNumber] = data.debug;
-
-		toClearCoords[turnNumber] = data.toClear;
-
-		flaggedCoords[turnNumber] = [];
-		if(data.debug && data.debug.cellInfo) {
-			$.each(data.debug.cellInfo, (key, cellInfo) => {
-				if(flaggedIndicator(cellInfo))
-					flaggedCoords[turnNumber].push(cellInfo.coords);
-			});
-		}
-	});
-
-	let $gameTable = $("<table>");
-
-	for(let i = 0; i < dims[0]; i++) {
-		gameGrid[i] = [];
-		let $row = $("<tr>");
-		$gameTable.append($row);
-
-		for(let j = 0; j < dims[1]; j++) {
-			gameGrid[i][j] = new GameCell([i, j]);
-			$row.append(gameGrid[i][j].$elm);
+		if(gameOver) {
+			this.gameOver = true;
+			showMsg(win ? "Win!!!1" : "Lose :(((");
 		}
 	}
 
-	$gameArea.append($("<ol>").attr("id", "turnList").addClass("laminate"));
-	$gameArea.append(
-		$("<div>").attr("id", "debugArea").append(
-			$("<div>").attr("id", "debugAreaTurn"),
-			$("<div>").attr("id", "debugAreaCell")
-		)
-	);
+	updateDebug({turnNumber : turn, debug, toClearCoords}) {
+		/* How the client indicates a cell is flagged (v. specific to python
+		client). */
+		const flaggedIndicator = info => info._state === "State.MINE";
 
-	this.close = () => {
+		this.debugInfo[turnNumber] = debug;
+
+		this.toClearCoords[turnNumber] = toClearCoords;
+
+		this.flaggedCoords[turnNumber] = [];
+		if(debug && debug.cellInfo) {
+			$.each(debug.cellInfo, (key, cellInfo) => {
+				if(flaggedIndicator(cellInfo))
+					this.flaggedCoords[turnNumber].push(cellInfo.coords);
+			});
+		}
+	}
+
+	clearCells(cells) {
+		if(cells.length === 0)
+			return;
+
+		if(!this.pass)
+			throw new Error(`Don't have the password for game '${this.id}'`);
+
+		serverAction("turn", {
+			id: this.id,
+			pass: this.pass,
+			coords: cells.map(c => c.coords)
+		});
+	}
+
+	getCell([x, y]) {
+		return this.gameGrid[x][y];
+	}
+
+	get inPlayState() {
+		return (
+			this.pass &&
+			!this.gameOver &&
+			this.currentTurn === this.latestTurn
+		);
+	}
+
+	close() {
 		$gameArea.empty();
 		$("#gameInfo").hide();
-		serverWatcher.close();
+		this.serverWatcher.close();
+	}
+}
+
+class GameCell {
+	constructor(game, coords) {
+		this.game = game;
+		this.coords = coords;
+
+		this.$elm = $("<td>").addClass("cell laminate");
+
+		this.changeState('unknown');
+	}
+
+	get surroundingCells() {
+		if(!this._surroundingCells) {
+			this._surroundingCells = [];
+
+			for (let i of [-1, 0, 1]) {
+				for (let j of [-1, 0, 1]) {
+					if(i === 0 && j === 0)
+						continue;
+
+					let x = this.coords[0] + i, y = this.coords[1] + j;
+
+					if(
+						x < 0 ||
+						y < 0 ||
+						x > this.game.dims[0] - 1 ||
+						y > this.game.dims[1] - 1
+					)
+						continue;
+
+					this._surroundingCells.push(this.game.getCell([x, y]));
+				}
+			}
+		}
+
+		return this._surroundingCells;
+	}
+
+	hover(hoverOn) {
+		this.$elm.toggleClass(
+			"cellHover",
+			hoverOn && this.state === cellState.UNKNOWN
+		);
+	}
+
+	hoverSurrounding(hoverOn) {
+		for(const cell of this.surroundingCells)
+			cell.hover(hoverOn);
+	}
+
+	clearSurrounding() {
+		this.hoverSurrounding(false);
+
+		this.game.clearCells(
+			this.surroundingCells.filter(c => c.state === cellState.UNKNOWN)
+		);
+	}
+
+	get debug() {
+		return this.game.debugInfo[this.currentTurn]
+			.cellInfo[this.coords.toString()];
+	}
+
+	changeState(newStateName, surrCount) {
+		const states = {
+			flagged : {
+				cellState : cellState.FLAGGED,
+				class : 'cellFlagged',
+				contextmenu : () => { this.changeState('unknown'); },
+			},
+			mine : {
+				class : 'cellMine'
+			},
+			unknown : {
+				cellState : cellState.UNKNOWN,
+				class : 'cellUnknown',
+				click : () => {
+					this.hover(false);
+					this.game.clearCells([ this ]);
+				},
+				contextmenu : () => { this.changeState('flagged'); },
+				mouseover : () => { this.hover(true); },
+				mouseout : () => { this.hover(false); }
+			},
+			cleared : {
+				cellState : surrCount,
+				class : 'cellCleared',
+				text : surrCount > 0 ? surrCount : undefined,
+				click : surrCount > 0 ? () => {
+					this.clearSurrounding();
+				} : undefined,
+				mouseover : surrCount > 0 ?
+					() => { this.hoverSurrounding(true); } : undefined,
+				mouseout : surrCount > 0 ?
+					() => { this.hoverSurrounding(false); } : undefined
+			},
+			toClear : {
+				class : 'cellToClear'
+			}
+		};
+
+		const newState = states[newStateName];
+		if(!newState)
+			throw new Error(`unexpected cell state: "${newStateName}"`);
+
+		/* Reverse any current mouseover effect */
+		this.$elm.mouseout();
+
+		this.$elm.off();
+		this.$elm.text("");
+
+		for(const s in states) {
+			if(states[s] !== newState && states[s].class)
+				this.$elm.removeClass(states[s].class);
+		}
+
+		this.state = newState.cellState;
+		this.$elm.addClass(newState.class);
+		this.$elm.text(newState.text);
+
+		/* Apply mouse actions to cell */
+		for (let mouseAction of [
+			'click',
+			'contextmenu',
+			'mouseover',
+			'mouseout',
+			'mouseup'
+		]) {
+			if(newState[mouseAction]) {
+				this.$elm.on(mouseAction, () => {
+					if(this.game.inPlayState)
+						newState[mouseAction]();
+				});
+			}
+		}
+
+		if(this.game.showDebug) {
+			this.$elm.on('click', () => {
+				displayDebug($("#debugAreaCell"), () => this.debug);
+			});
+		}
+
+		/* TODO: this is meant to highlight surrounding cells right after
+		clicking an unknown cell. Doesn't work (:hover is false); don't know
+		why. */
+		// if(this.$elm.is(":hover"))
+		// 	this.$elm.mouseover();
 	}
 }
